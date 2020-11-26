1ddde31b7179d5936b232ed99c0a20be20991084
whee <wheesys@sina.com>
2020-11-26 23:11:41 +0800

    change to table

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,24 +1,106 @@
 <template>
-  <div class="main_app">
-    <a-card title="Jaycee Chow">
-      a good coder
-    </a-card>
+  <div>
+    <a-button class="editable-add-btn" @click="handleAdd">
+      Add
+    </a-button>
+    <a-table
+      :columns="columns"
+      :row-key="record => record.login.uuid"
+      :data-source="data"
+      :pagination="pagination"
+      :loading="loading"
+      @change="handleTableChange"
+      style="min-width: 800px"
+      :showHeader="false"
+    >
+      <template slot="name" slot-scope="name">
+        {{ name.first }} {{ name.last }}
+      </template>
+      <template slot="operation" slot-scope="text, record">
+        <a-popconfirm
+          v-if="dataSource.length"
+          title="Sure to delete?"
+          @confirm="() => onDelete(record.key)"
+        >
+          <a href="javascript:">Delete</a>
+        </a-popconfirm>
+      </template>
+    </a-table>
   </div>
 </template>
+<script>
+import reqwest from "reqwest";
 
-<script lang="ts">
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    sorter: true,
+    width: "20%",
+    scopedSlots: { customRender: "name" }
+  },
+  {
+    title: "Gender",
+    dataIndex: "gender",
+    filters: [
+      { text: "Male", value: "male" },
+      { text: "Female", value: "female" }
+    ],
+    width: "20%"
+  },
+  {
+    title: "Email",
+    dataIndex: "email"
+  }
+];
+
 export default {
-  name: "app"
+  data() {
+    return {
+      data: [],
+      pagination: {},
+      loading: false,
+      columns
+    };
+  },
+  mounted() {
+    this.fetch();
+  },
+  methods: {
+    handleTableChange(pagination, filters, sorter) {
+      console.log(pagination);
+      const pager = { ...this.pagination };
+      pager.current = pagination.current;
+      this.pagination = pager;
+      this.fetch({
+        results: pagination.pageSize,
+        page: pagination.current,
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...filters
+      });
+    },
+    fetch(params = {}) {
+      console.log("params:", params);
+      this.loading = true;
+      reqwest({
+        url: "https://randomuser.me/api",
+        method: "get",
+        data: {
+          results: 10,
+          ...params
+        },
+        type: "json"
+      }).then(data => {
+        const pagination = { ...this.pagination };
+        // Read total count from server
+        // pagination.total = data.totalCount;
+        pagination.total = 200;
+        this.loading = false;
+        this.data = data.results;
+        this.pagination = pagination;
+      });
+    }
+  }
 };
 </script>
-
-<style>
-.main_app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import AppComponent from "./App/App.vue";
-import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-Vue.use(Antd);
+import Antd, {Checkbox, List} from "ant-design-vue";
 
 Vue.config.productionTip = false;
 Vue.component("app-component", AppComponent);
+Vue.use(List);
+Vue.use(Antd);
+Vue.use(Checkbox);
 
 new Vue({
   el: "#app",
